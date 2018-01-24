432e177919a12e61457655be98a1ce783b26ec2c
richardse08 <eric.richards.finance@gmail.com>
2018-01-24 13:14:44 -0500

    Set up environment

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-var express = require(“express”);
+var express = require('express');
 var app = express();
 
 var port = process.env.PORT || 3000;
@@ -6,9 +6,22 @@ var port = process.env.PORT || 3000;
 // This will serve the public docs
 app.use('/', express.static(__dirname + '/public'));
 
-// Render homepage
-app.get('/', function(request,response) {
+
+
+app.set("view engine", "ejs"); // Set view engine and give extension
+app.set("views", "./views"); // Rename views a folder called views
+
+
+
+// Render ejs for homepage
+app.get("/", function(request, response) {
+
+    // Use render to run through ejs and then send up the index.ejs (which is my homepage)
     response.render('index');
+
 });
+
+
+
 
 app.listen(port); //Open browser to “localhost:<port>” to view pages
