2f80fb8c40cf2f1620db9a6d302fe7147b7af808
richardse08 <eric.richards.finance@gmail.com>
2018-01-27 17:03:39 -0500

    Configuring heroku

--- server.js
+++ server.js
@@ -1,20 +1,16 @@
 var express = require('express');
 var app = express();
-
 var port = process.env.PORT || 3000;
 
 // This will serve the public docs
 app.use('/', express.static(__dirname + '/public'));
-
-
-
-app.set("view engine", "ejs"); // Set view engine and give extension
-app.set("views", "./views"); // Rename views a folder called views
+app.set('view engine', 'ejs'); // Set view engine and give extension
+app.set('views', './views'); // Rename views a folder called views
 
 
 
 // Render ejs for homepage
-app.get("/", function(request, response) {
+app.get('/', function(request, response) {
 
     // Use render to run through ejs and then send up the index.ejs (which is my homepage)
     response.render('index');
