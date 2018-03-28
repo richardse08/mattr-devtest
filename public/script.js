c38d8a1b0b5298b8e35d1c7505b34258479b4025
richardse08 <eric.richards.finance@gmail.com>
2018-03-28 12:59:56 -0400

    Configured basic X button functionality for lightbox

--- public/script.js
+++ public/script.js
@@ -3,22 +3,22 @@ $(document).ready(function() {
 
 
 
-        // // Add overlay and pop up modal when lightbox is clicked
-        // $('.lightbox-listener').on('click', function() {
-        //     $('.lightbox').removeClass('display-none');
-        //     $('.background-overlay').removeClass('display-none');
+        // Add overlay and pop up modal when lightbox is clicked
+        $('.lightbox-listener').on('click', function() {
+            $('.lightbox').removeClass('display-none');
+            $('.background-overlay').removeClass('display-none');
     
-        //     $('html, body').animate({
-        //         // Scroll to degree, taking into account 64 pixels of the sticky nav
-        //         scrollTop: 0
-        //     }, 'slow');
-        // });
+            $('html, body').animate({
+                // Scroll to degree, taking into account 64 pixels of the sticky nav
+                scrollTop: 0
+            }, 'slow');
+        });
     
-        // // Remove overlay and remove pop up modal when exit button is clicked
-        // $('.lightbox-exit').on('click', function() {
-        //     $('.lightbox').addClass('display-none');
-        //     $('.background-overlay').addClass('display-none');
-        // });
+        // Remove overlay and remove pop up modal when exit button is clicked
+        $('.lightbox-exit').on('click', function() {
+            $('.lightbox').addClass('display-none');
+            $('.background-overlay').addClass('display-none');
+        });
 
 
 
