fd19899873218788cff7bdabc534332184ab2f66
richardse08 <eric.richards.finance@gmail.com>
2018-03-28 16:09:27 -0400

    check point

--- public/script.js
+++ public/script.js
@@ -3,112 +3,163 @@ $(document).ready(function() {
 
 
 
-        // Add overlay and pop up modal when lightbox is clicked
-        $('.lightbox-listener').on('click', function() {
-
-            // Get the html of the blocks user clicks on
-            var headerInject = this.children[1].innerHTML;
-            var paragraphInject = this.children[2].innerHTML;
-
-            // Dynamically inject that html into the modal on the fly
-            $('.screenshot__title--title').html(headerInject);
-            $('.screenshot__title--body').html(paragraphInject);
-
-            $('.lightbox').removeClass('display-none');
-            $('.background-overlay').removeClass('display-none');
+    window.onscroll = function() {
+        $('.nav').removeClass('display-none');
+    };
     
-            $('html, body').animate({
-                // Scroll to degree, taking into account 64 pixels of the sticky nav
-                scrollTop: 0
-            }, 'slow');
-        });
 
 
 
+
+
+
+    // Get sticky-nav
+    var js_nav = document.getElementById("js-nav");
+
+    // Get the offset position of the sticky-nav
+    var sticky = js_nav.offsetTop;
 
     
-        // Remove overlay and remove pop up modal when exit button is clicked
-        $('.lightbox-exit').on('click', function() {
-            $('.lightbox').addClass('display-none');
-            $('.background-overlay').addClass('display-none');
+
+
+
+
+
+    // Add/remove sticky class
+    function sticky_nav_scroll() {  
+        if (window.pageYOffset >= sticky) {
+            $('.nav').removeClass('display-none');
+        } 
+        
+        else {
+            $('.nav').addClass('display-none');
+        }
+        
+    } // end sticky_nav_scroll
+
+    // If user scrolls, fire scroll function
+    window.onscroll = function() {
+        sticky_nav_scroll();
+    };
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // Add overlay and pop up modal when lightbox is clicked
+    $('.lightbox-listener').on('click', function() {
+
+        // Get the html of the blocks user clicks on
+        var headerInject = this.children[1].innerHTML;
+        var paragraphInject = this.children[2].innerHTML;
+
+        // Dynamically inject that html into the modal on the fly
+        $('.screenshot__title--title').html(headerInject);
+        $('.screenshot__title--body').html(paragraphInject);
+
+        $('.lightbox').removeClass('display-none');
+        $('.background-overlay').removeClass('display-none');
+
+        $('html, body').animate({
+            // Scroll to degree, taking into account 64 pixels of the sticky nav
+            scrollTop: 0
+        }, 'slow');
+    });
+
+
+
+
+
+    // Remove overlay and remove pop up modal when exit button is clicked
+    $('.lightbox-exit').on('click', function() {
+        $('.lightbox').addClass('display-none');
+        $('.background-overlay').addClass('display-none');
+    });
+
+
+
+
+    function validateEmail (email) {
+        if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) {
+
+            // Send the email address to the emailSave function to send to the database
+            emailSave(email);
+
+            // Simple alert, need to change
+            alert('Your request has been saved. Thank you!');
+
+            // Remove display-none on checkbox
+            $('.checkbox').removeClass('display-none');
+
+            // Change submit button text
+            var requested = 'REQUESTED';
+            // $('invite__request--text').html(requested);
+            // invite__request--text.innerHTML(requested);
+        }
+        else {
+
+            // Simple alert, need to change
+            alert('Please enter a valid email address')
+        }
+    };
+
+
+
+
+
+
+
+    // Create function to save email and send it to database
+    function emailSave(email) {
+
+        // Create email for development only
+        var testEmail = 'eric.richards.finance@gmail.com';
+
+        // Send it
+        $.get( "/email-save", { emailAddress: email}, function(data) {
+        
+            console.log(data);
+            console.log('checkpoint');                
         });
 
+    };
 
 
 
-        function validateEmail (email) {
-            if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) {
+    // Click email link
+    $('.js-email-link').on('click', function() {
+        
+        // Get value of the input tag on the front end and save as a variable
+        var emailValue = $('.email__field').val();
 
-                // Send the email address to the emailSave function to send to the database
-                emailSave(email);
+        // Check if email is valid
+        validateEmail(emailValue);
 
-                // Simple alert, need to change
-                alert('Your request has been saved. Thank you!');
+        // Clear out the value of the input field
+        $('.email__field').val('');
 
-                // Remove display-none on checkbox
-                $('.checkbox').removeClass('display-none');
+        // Checkpoint
+        console.log('email has been clicked');
 
-                // Change submit button text
-                var requested = 'REQUESTED';
-                // $('invite__request--text').html(requested);
-                // invite__request--text.innerHTML(requested);
-            }
-            else {
-
-                // Simple alert, need to change
-                alert('Please enter a valid email address')
-            }
-        };
+    });
 
 
 
-
-
-
-
-        // Create function to save email and send it to database
-        function emailSave(email) {
-
-            // Create email for development only
-            var testEmail = 'eric.richards.finance@gmail.com';
-
-            // Send it
-            $.get( "/email-save", { emailAddress: email}, function(data) {
-            
-                console.log(data);
-                console.log('checkpoint');                
-            });
-
-        };
-
-
-
-        // Click email link
-        $('.js-email-link').on('click', function() {
-            
-            // Get value of the input tag on the front end and save as a variable
-            var emailValue = $('.email__field').val();
-
-            // Check if email is valid
-            validateEmail(emailValue);
-
-            // Clear out the value of the input field
-            $('.email__field').val('');
-
-            // Checkpoint
-            console.log('email has been clicked');
-
-        });
-
-
-
-        // Click enter key fire email function
-        $('.email__field').keyup(function (enterEvent) {
-            var key = enterEvent.which;
-            if(key == 13) {
-                $('.js-email-link').click();
-            }
-        });
+    // Click enter key fire email function
+    $('.email__field').keyup(function (enterEvent) {
+        var key = enterEvent.which;
+        if(key == 13) {
+            $('.js-email-link').click();
+        }
+    });
 
 
 
