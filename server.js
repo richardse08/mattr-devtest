084b2680bca1a658150d30452160e0e56e63f116
richardse08 <eric.richards.finance@gmail.com>
2018-03-30 18:59:04 -0400

    check point

--- server.js
+++ server.js
@@ -3,16 +3,11 @@ var app = express();
 var emailDump;
 var port = process.env.PORT || 3000;
 
-
-
-
-
 // Configure Mongo database
 var MongoClient = require('mongodb').MongoClient; // Require mongodb
 var doesCollectionExist = true; // Set checker tool so only one document is made
-var url = "mongodb://localhost:27017/mydb"; // Set a URL for my database
-// var url = 'mongodb://mattr-username:mattr-password1@ds137246.mlab.com:37246/mattr-database';
-
+// var url = "mongodb://localhost:27017/mydb"; // Set a URL for my database
+var url = 'mongodb://mattr-username:mattr-password1@ds137246.mlab.com:37246/mattr-database';
 
 // Connect to database
 MongoClient.connect(url, function(err, db) {
@@ -25,13 +20,10 @@ MongoClient.connect(url, function(err, db) {
 });
 
 
-
 // This will serve the public docs
 app.use('/', express.static(__dirname + '/public'));
 app.set('view engine', 'ejs'); // Set view engine and give extension
 app.set('views', './views'); // Rename views a folder called views
-
-
 
 // Render ejs for homepage
 app.get('/', function(request, response) {
@@ -40,7 +32,6 @@ app.get('/', function(request, response) {
     response.render('index');
 
 });
-
 
 // API to receive AJAX hits from client ,this inserts the data object into emailDump
 app.get("/email-save", function(request, response) {    
@@ -85,28 +76,10 @@ app.get("/email-save", function(request, response) {
             { upsert: true }
         );
     }
-    
-    
+
     response.send("success");
 
-    
 }); // End API
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 app.listen(port); //Open browser to “localhost:<port>” to view pages
